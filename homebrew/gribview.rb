ac2862a76621ecb63a1e5e152d8e42ccd053fd3b
Jean-Baptiste Filippi <batti.filippi@gmail.com>
2025-12-04 08:21:20 +0100

    u

--- homebrew/gribview.rb
+++ homebrew/gribview.rb
@@ -5,6 +5,11 @@ class Gribview < Formula
   sha256 "4cc8e8e9b2d407efe8957de9ab705e18839d459a1596a136476094bdce7ca6cb"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/filippi/gribview/releases/download/v1.0"
+    sha256 cellar: :any, arm64_sequoia: "4d466672cf099dc6a0156aa8a30ce1893083cb64e15c9df0e92fc938c87cfb00"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "eccodes"
